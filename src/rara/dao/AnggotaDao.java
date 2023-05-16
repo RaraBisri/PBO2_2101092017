8973cdd677b664e5f345910229af6ba2d7d654c9
RaraBisri <rarabisri@gmail.com>
2023-05-16 09:24:08 +0800

    Create AnggotaDao.java

---
+++ src/rara/dao/AnggotaDao.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package rara.dao;
+
+import java.util.List;
+import java.sql.Connection;
+import rara.model.Anggota;
+
+/**
+ *
+ * @author USER
+ */
+public interface AnggotaDao {
+    public void insert(Connection con, Anggota anggota) throws Exception;
+    public void update(Connection con, Anggota anggota) throws Exception;
+    public void delete(Connection con, Anggota anggota) throws Exception;
+    public Anggota getAnggota(Connection con,String kode) throws Exception;
+    public List<Anggota> getAllAnggota(Connection con) throws Exception;
+}
