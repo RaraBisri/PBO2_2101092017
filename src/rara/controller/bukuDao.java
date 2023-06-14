9774fdf9f71db3ad1b2ed95c3751e6507195ff0c
RaraBisri <rarabisri@gmail.com>
2023-06-14 12:47:59 +0800

    Create bukuDao.java

---
+++ src/rara/controller/bukuDao.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package rara.controller;
+
+import java.sql.Connection;
+import java.util.List;
+import rara.model.Buku;
+
+/**
+ *
+ * @author USER
+ */
+class bukuDao {
+
+    static List<Buku> getAllBuku(Connection con) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
