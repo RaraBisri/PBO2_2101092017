9821183ca69d7493f74a1c64dab50185d862fe49
RaraBisri <rarabisri@gmail.com>
2023-06-14 12:48:29 +0800

    Update Buku.java

--- src/rara/model/Buku.java
+++ src/rara/model/Buku.java
@@ -56,6 +56,10 @@ public class Buku {
     public void setPenerbit(String penerbit) {
         this.penerbit = penerbit;
     }
+
+    public void setTglkembali(String string) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
     
     
 }
